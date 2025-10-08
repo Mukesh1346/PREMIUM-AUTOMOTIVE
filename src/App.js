87bed68e51361e60d523cd6715344c7b642d2062
Mukesh1346 <mukeshmahar00@gmail.com>
2025-10-08 11:59:12 +0530

    show this loader on Refresh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Home from './Pages/Home';
-import StartScreen from './Components/StartScreen/StartScreen' 
+import StartScreen from './Components/StartScreen/StartScreen';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import AboutUs from './Components/AboutUs/AboutUs';
@@ -12,17 +12,9 @@ import ContactUs from './Components/ContactUs/ContactUs';
 import OurNetworks from './Components/OurNetworks/OurNetworks';
 
 function App() {
-  const [showStart, setShowStart] = useState(false);
-
-  useEffect(() => {
-    const hasStarted = localStorage.getItem('hasStarted');
-    if (!hasStarted) {
-      setShowStart(true);
-    }
-  }, []);
+  const [showStart, setShowStart] = useState(true);
 
   const handleStart = () => {
-    localStorage.setItem('hasStarted', 'true');
     setShowStart(false);
   };
 
@@ -32,7 +24,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
@@ -40,11 +32,71 @@ function App() {
         <Route path="/brands" element={<BrandsUs />} />
         <Route path="/contactus" element={<ContactUs />} />
         <Route path="/our-networks" element={<OurNetworks />} />
-        {/* Add more routes here later if needed */}
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import Home from './Pages/Home';
+// import StartScreen from './Components/StartScreen/StartScreen' 
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+// import Navbar from './Components/Navbar/Navbar';
+// import Footer from './Components/Footer/Footer';
+// import AboutUs from './Components/AboutUs/AboutUs';
+// import Mission from './Components/Mission/Mission';
+// import BrandsUs from './Components/Brands/Brands';
+// import ContactUs from './Components/ContactUs/ContactUs';
+// import OurNetworks from './Components/OurNetworks/OurNetworks';
+
+// function App() {
+//   const [showStart, setShowStart] = useState(false);
+
+//   useEffect(() => {
+//     const hasStarted = localStorage.getItem('hasStarted');
+//     if (!hasStarted) {
+//       setShowStart(true);
+//     }
+//   }, []);
+
+//   const handleStart = () => {
+//     localStorage.setItem('hasStarted', 'true');
+//     setShowStart(false);
+//   };
+
+//   if (showStart) {
+//     return <StartScreen onStart={handleStart} />;
+//   }
+
+//   return (
+//     <BrowserRouter>
+//       <Navbar/>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/aboutus" element={<AboutUs />} />
+//         <Route path="/mission" element={<Mission />} />
+//         <Route path="/brands" element={<BrandsUs />} />
+//         <Route path="/contactus" element={<ContactUs />} />
+//         <Route path="/our-networks" element={<OurNetworks />} />
+//         {/* Add more routes here later if needed */}
+//       </Routes>
+//       <Footer/>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
